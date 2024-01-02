234dbe60598c6cc2630f29b1afb9797315467393
Bhagyashree Ganesh Tekade <bhagyashreetekade609@gmail.com>
2024-01-03 01:05:55 +0530

    done

--- Sorting algo/merge_sort.cpp
+++ Sorting algo/merge_sort.cpp
@@ -1,64 +1,55 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-void merge(int a[],int lb,int mid,int ub){
-	
-	int i=lb;
-	int j=mid+1;
-	int k=lb;
-	int b[ub+1];
-	
-	while(i<=mid && j<=ub){
-		if(a[i]<a[j]){
-			a[k]=a[i];
-			i++;
-		}
-		else{
-			a[k]=a[j];
-			j++;
-		}
-		k++;
-	}
-	
-	if(i>mid){
-		while(j<=ub){
-			a[k]=a[j];
-			j++;
-			k++;
-		}
-	}
-	else{
-		while(i<mid){
-			a[k]=a[i];
-			i++;
-			k++;
-		}
-	}
-	
-	for(int i=0;i<=ub;i++){
-		a[i]=b[i];
-	}
+void merge(vector<int> &arr, int low, int mid, int high) {
+    vector<int> temp;
+    int left = low, right = mid + 1;
+
+    while (left <= mid && right <= high) {
+        if (arr[left] <= arr[right]) {
+            temp.push_back(arr[left]);
+            left++;
+        } else {
+            temp.push_back(arr[right]);
+            right++;
+        }
+    }
+
+    while (left <= mid) {
+        temp.push_back(arr[left]);
+        left++;
+    }
+
+    while (right <= high) {
+        temp.push_back(arr[right]);
+        right++;
+    }
+
+    for (int i = low; i <= high; i++) {
+        arr[i] = temp[i-low];
+    }
+
 }
 
-void mergesort(int a[],int lb,int ub){
-	
-	if(lb<ub){
-		int mid=(lb+ub)/2;
-		mergesort(a,lb,mid);
-		mergesort(a,mid+1,ub);
-		merge(a,lb,mid,ub);
-	}
+void printarr(vector<int> &arr){
+    for(int i=0;i<arr.size();i++){
+        cout<<arr[i]<<" ";
+    }
 }
 
-void printarr(int a[],int n){
-	for(int i=0;i<n;i++){
-		cout<<a[i]<<" ";
-	}
+void merge_sort(vector<int> &arr, int low, int high) {
+
+    if (low < high) {
+        int mid = (low + high) / 2;
+        merge_sort(arr, low, mid);
+        merge_sort(arr, mid + 1, high);
+        merge(arr, low, mid, high);
+    }
 }
 
-int main(){
-	int a[5]={5,8,2,1,9};
-	mergesort(a,0,4);
-	printarr(a,5);
-	
+int main() {
+    vector<int> arr = {3, 7, 2,7, 8};
+    merge_sort(arr, 0, arr.size() - 1);
+    printarr(arr);
 }
