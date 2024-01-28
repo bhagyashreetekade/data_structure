56f1e1e8184c58e9d21044d4b0d6acc0566f1beb
Bhagyashree Ganesh Tekade <bhagyashreetekade609@gmail.com>
2024-01-28 11:09:40 +0530

    added doubly linked list

--- Linked list/doubly_linked_list.cpp
+++ Linked list/doubly_linked_list.cpp
@@ -32,17 +32,47 @@ node* convert2DoublyLL(vector<int>& arr){
     return head;
 }
 
+void insertAtTail(node* &head,int val){
+    node* n=new node(val);
+    node* temp=head;
+    
+    if(head==NULL){
+        head=n;
+    }
+    else {
+        while(temp->next!=NULL){
+        temp=temp->next;
+    }
+        temp->next=n;
+        n->prev=temp;
+    }
+}
+
+void insertAtHead(node* &head,int val){
+    node* n=new node(val);
+    node* temp=head;
+    n->next=temp;
+    head=n;
+    temp->prev=n;
+}
+
 void printlist(node* head){
     while(head!=NULL){
         cout<<head->data<<" ";
         head=head->next;
     }
+    cout<<endl;
 }
 
 int main() {
     vector<int>arr={2,4,6,8};
-    
     node* head=convert2DoublyLL(arr);
+    cout<<"Convert vector to linked list : ";
     printlist(head);
-    
+    node* n=NULL;
+    insertAtTail(n,11);
+    insertAtTail(n,12);
+    insertAtTail(n,13);
+    insertAtHead(n,10);
+    printlist(n);
 }
